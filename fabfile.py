5ec65d7f8eabfb8bc252653308fd0dc81b819fca
Tyler Long <tyler4long@gmail.com>
2015-03-18 17:08:36 +0800

    Add mermaid dist files

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+import json
+from fabric.api import local
+
+
+def update():
+    local('rm *.js')
+    local('rm *.css')
+    local('wget https://github.com/knsv/mermaid/archive/master.zip')
+    local('unzip master.zip && rm master.zip')
+    latest_version = json.loads(open('mermaid-master/bower.json').read())['version']
+    local('cp mermaid-master/dist/mermaid.css .')
+    local('cp mermaid-master/dist/mermaid.full.min.js .')
+    local('rm -rf mermaid-master')
+    print 'latest version: {0}'.format(latest_version)
